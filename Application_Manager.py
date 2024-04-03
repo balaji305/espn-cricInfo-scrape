935cf6d4a550a570f26594f026358b61325f039d
badguy-00@github.com <balajisankar555@gmail.com>
2024-04-03 23:00:10 +0530

    Bat vs Bowl

--- Application_Manager.py
+++ Application_Manager.py
@@ -2,6 +2,7 @@ import Fantasy_Point_Table
 import Player_Table
 import Match_Table
 import Report_Generation
+import Bat_vs_Bowl
 import os
 
 choice = -1
@@ -17,7 +18,8 @@ while(True):
     print("4. Upload into Every Table")
     print("5. Upload and Generate Report")
     print("6. Generate Report only")
-    print("7. Exit")
+    print("7. Bat vs Bowl Report")
+    print("8. Exit")
     print("-------------------------------------------------------")
     choice = int(input("Enter your choice: "))
     print("-------------------------------------------------------")
@@ -70,6 +72,11 @@ while(True):
         print("Report Generated Successfully!!\n")
         print("Please check the Output File to know the results.")
     elif(choice == 7):
+        print("Generating Bat vs Bowl Report...\n")
+        Bat_vs_Bowl.Bat_vs_Bowl_Call()
+        print("Report Generated Successfully!!\n")
+        print("Please check the Output File to know the results.")
+    elif(choice == 8):
         print("Thank You for using Fantasy Cricket League Application Manager")
         nxt = input("")
         break
